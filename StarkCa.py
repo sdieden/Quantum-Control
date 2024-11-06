29d48d6900a463434ece86d200bbc4db70d57888
sdieden <samdieden21@gmail.com>
2024-11-06 16:17:09 +0100

    ajout de deux fichiers importants du module ARC. Un est légèrement modifié pour laisser la possibiltié de calculer les défauts quantiques quand l'information est manquante. version avaancée de starkmap, il reste des problèmes d'indices + manque des triplets ?

--- StarkCa.py
+++ StarkCa.py
@@ -4,6 +4,7 @@ from arc import *
 import numpy as np
 import matplotlib.pyplot as plt
 from decimal import Decimal
+from holoviews.plotting.bokeh.styles import font_size
 from sympy import S
 from twisted.protocols.amp import Decimal
 
@@ -13,47 +14,61 @@ calc = StarkMap(Calcium40())
 
 # Target state
 n0 = 35
-l0 = 1
-j0 = 1
+l0 = 3
+j0 = 3
 mj0 = 0
 # Define max/min n values in basis
-nmin = n0 - 7
-nmax = n0 + 7
+nmin = n0 - 5
+nmax = n0 + 5
 # Maximum value of l to include (l~20 gives good convergence for states with l<5)
 lmax = 34
 
 # Initialise Basis States for Solver : progressOutput=True gives verbose output
-calc.defineBasis(n0, l0, j0, mj0, nmin, nmax, lmax, progressOutput=True, s=1)
+calc.defineBasis(n0, l0, j0, mj0, nmin, nmax, lmax, progressOutput=True, s=0)
 
 Emin = 0.0  # Min E field (V/m)
-Emax = 4.0e2  # Max E field (V/m)
+Emax = 8.0e2  # Max E field (V/m)
 N = 1001  # Number of Points
 
 # Generate Stark Map
 calc.diagonalise(np.linspace(Emin, Emax, N), progressOutput=True)
 # Show Sark Map
 calc.plotLevelDiagram(progressOutput=True, units=0, highlightState=True)
-calc.ax.set_ylim(-101.25,-100 )
+calc.ax.set_ylim(-105,-70 )
 calc.showPlot(interactive=False)
+
+#####
+#Other method to calcul mixing states composition using directly made in ARC
+
 # return the ensemble of mixing states, for small E, we should get 1 for the initial state and 0 for the others
-
-
 # we want that it shows the quantum numbers associated to every value calculated, to make it more readable, we can just
 # show non 0 values.
 a=calc.highlight[N-1]
-MixingStates_Coef = []#np.array([])
+print('a=',a)
+MixingStates_Coef = []#list
 
-MixingStates_Term = []#np.array([])
+MixingStates_Term = []#list
+for i in range(len(a)):
+    if calc.basisStates[i] == [35, 3, 3, 0] :
+        print('Il est ici!!!!',i)
+    else :
+        pass
 for i in range(len(a)):
     if a[i] > 10**-3 :
+        print('i=',i)
         #print("{}\t{}".format(calc.basisStates[i],a[i]))
-        print(calc.basisStates[i])
+        print(len(a))
+        print(len(calc.basisStates))
+        print(calc.basisStates)
 
         MixingStates_Coef.append(a[i])
         MixingStates_Term.append(calc.basisStates[i])
+        #print(calc.basisStates[i],'i')
+        #print(calc.basisStates[i-1],'i-1')
+        #print(calc.basisStates[i + 1], 'i+1')
     else :
         pass
-#print(MixingStates_Coef)
+print('coef=',MixingStates_Coef)
 # still need to check that highlight and basisStates are arranged the same way.
 
 # histogramm of the mixingstates
@@ -61,8 +76,8 @@ for i in range(len(a)):
 n_bins = len(MixingStates_Coef)
 MixingStates_Coef = np.array(MixingStates_Coef)
 MixingStates_Term = np.array(MixingStates_Term)
-for i in range(len(MixingStates_Coef)):
-    MixingStates_Coef[i] = '%.2E' % Decimal(MixingStates_Coef[i])
+#for i in range(len(MixingStates_Coef)):
+#    MixingStates_Coef[i] = '%.2E' % Decimal(MixingStates_Coef[i])
 # make data:
 ## separation of information for the term matrix
 #x = []
@@ -97,10 +112,13 @@ def Term(n,l,j,s):
     return term.format(N=int(first_quantum_number),J = int(j),L = orbital,S=int(Spin_part))
 # now we rewrite the list of list as a list of terms
 MixingStates_Term_new = []
+print(MixingStates_Term)
 for i in range(len(MixingStates_Term)):
 
     MixingStates_Term_new.append(Term(MixingStates_Term[i][0],MixingStates_Term[i][1],MixingStates_Term[i][2],MixingStates_Term[i][3]))
+
 x = MixingStates_Term_new
+print(x)
 y = MixingStates_Coef
 
 # plot
@@ -110,7 +128,7 @@ multiplier = 0
 for coef in MixingStates_Coef:
     offset = multiplier
     rects = ax.bar(offset,coef ,width=1)
-    ax.bar_label(rects, padding=5, fontsize=8)
+    ax.bar_label(rects,labels=[f"{coef:.3f}" for coef in rects.datavalues], padding=5, fontsize=8, rotation='vertical')
     multiplier+=1
 # length of axes should be adjusted to the number of relevant states in the mixing state
 ax.set(xlim=(-0.5, len(MixingStates_Coef)-0.5),
@@ -118,6 +136,6 @@ ax.set(xlim=(-0.5, len(MixingStates_Coef)-0.5),
 ax.set_ylabel('coefficient du mix')
 ax.set_title(r'Mixing states of Stark Levels in $^{40}Ca$')
 ax.tick_params(axis='x',pad=0)
-plt.xticks(rotation = 90)
+plt.xticks(font_size=8,rotation = 90)
 plt.show()
 print(Term(n0,l0,j0,s=1))
