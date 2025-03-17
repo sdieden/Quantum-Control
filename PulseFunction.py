48445763e8cf45dffc1c31dd2cf37e6cbbd11906
sdieden <75951267+sdieden@users.noreply.github.com>
2025-03-17 16:04:54 +0100

    works,written in a.u.

--- PulseFunction.py
+++ PulseFunction.py
@@ -14,7 +14,7 @@ from arc_sam import *  # Import ARC (Alkali Rydberg Calculator)
 
 atom = Calcium40()
 calc = StarkMap(atom)
-hbar = 1
+
 n = 35
 l = 3
 j = 3
@@ -22,7 +22,20 @@ mj = 0
 s = 0
 nmin = n - 1
 nmax = n + 1
-lmax = nmax - 1
+lmax = 5
+
+"""
+Rappel des unités atomiques  : 
+action : hbar = 1 , hbar = 1.1*10^-34 J.s
+energie : hartree, 1 Hartree = 4.359*10^-18 J
+temps : 1 t.ua = 2.418*10^-17 s
+champ électrique : 1 ce.ua = 5.14*10^11V/m
+
+"""
+conv_en = 4.359e18
+conv_t = 2.418e17
+conv_ce = 5.14e-11
+hbar = 1
 calc.defineBasis(n,l,j,mj,nmin,nmax,lmax,s=s)
 def U(en,dt):
     """
@@ -39,22 +52,36 @@ def U(en,dt):
     return u
 def apply_pulse(psi_in,pulse):
 
-    psi_out = np.zeros(len(psi_in))
-    psi_out = np.complex128(psi_out)
-    if pulse.stark == True:
-        for state in psi_in:
-            for substate in range(len(calc.composition[pulse.index_of_amplitude()][state])):
-                coef = psi_in[substate]*calc.composition[pulse.index_of_amplitude()][state][substate]
-                en = calc.y[pulse.index_of_amplitude()][substate]
-                coef *= U(en,pulse.duration)
-                psi_out[calc.composition[pulse.index_of_amplitude()][state][1]] += coef
+    psi_out = np.zeros(len(psi_in),dtype=np.complex128)
 
+    if pulse.stark == True:
+        for state in range(len(psi_in)):
+            coef_state = psi_in[state]
+            """if len(calc.composition[pulse.index_of_amplitude()][state]) == 1 :
+                coef_substate = calc.composition[pulse.index_of_amplitude()][state][0][0]
+                en = calc.y[pulse.index_of_amplitude()][state]
+                value = coef_state*coef_substate
+                value *= U(en,pulse.duration)
+                psi_out[calc.composition[pulse.index_of_amplitude()][state][0][1]] += value
+            else:
+                for substate in range(len(calc.composition[pulse.index_of_amplitude()][state])):
+                    coef = psi_in[substate]*calc.composition[pulse.index_of_amplitude()][state][substate][0]
+                    en = calc.y[pulse.index_of_amplitude()][substate]
+                    coef *= U(en,pulse.duration)
+                    psi_out[calc.composition[pulse.index_of_amplitude()][state][0][1]] += coef
+            """
+            for substate in range(len(calc.composition[pulse.index_of_amplitude()][state])):
+                coef_substate = calc.composition[pulse.index_of_amplitude()][state][substate][0]
+                en = calc.y[pulse.index_of_amplitude()][substate] * conv_en
+                value = coef_state*coef_substate
+                value *= U(en,pulse.duration*conv_t)
+                psi_out[calc.composition[pulse.index_of_amplitude()][state][substate][1]] += value
 
     elif pulse.stark == False:
-        for state in psi_in:
+        for state in range(len(psi_in)):
             coef = psi_in[state]
-            en = calc.y[0][state]
-            coef *= U(en,pulse.duration)
+            en = calc.y[0][state]*conv_en
+            coef *= U(en,pulse.duration*conv_t)
             psi_out[state] += coef
 
     return psi_out
@@ -85,8 +112,8 @@ def pulse_evolution(pulseList):
         """
 
 # initial state
-    initial_psi = np.zeros(len(calc.basisStates))
-    initial_psi = np.complex128(initial_psi)
+    initial_psi = np.zeros(len(calc.basisStates),dtype=np.complex128)
+
     #initial_psi[calc.index_new_basis[calc.indexOfCoupledState]] = 1
     initial_psi[calc.indexOfCoupledState] = 1
 #
@@ -94,12 +121,24 @@ def pulse_evolution(pulseList):
 
     for pulse in Pulse.liste_pulse:
         idx=pulse.index_of_amplitude()
-    calc.diagonalise(Pulse.amplitudes())
+    calc.diagonalise(sorted(tuple(pulse.amplitudes_list)),upTo=-1)
     for i in range(len(Pulse.liste_pulse)):
         psi.append(apply_pulse(psi[-1], pulse))
     
     return psi
 
+def total_population(psi_array):
+    abs_array = np.abs(psi_array)
+    population = np.zeros(len(psi_array))
+    for k in range(len(abs_array)):
+        population[k] = np.sum(abs_array[k] ** 2)
+
+    return population
+
 list = [(30,5),(0,10),(20,1)]
 
-pulse_evolution(list)
+a=pulse_evolution(list)
+peuple = total_population(a)
+print('population=',peuple,'final state=',a[-1])
+
+
