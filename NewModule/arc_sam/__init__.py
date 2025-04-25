eb392390f8207dcc4e2a10f128733aedf1322c30
sdieden <75951267+sdieden@users.noreply.github.com>
2025-04-25 15:26:06 +0200

    updating files

---
+++ NewModule/arc_sam/__init__.py
@@ -0,0 +1,105 @@
+# Google-style docstrings are used for documenting code
+# for more on those, see http://www.sphinx-doc.org/en/stable/ext/example_google.html#example-google
+# this is automatically parsed by Aphinx module in Python, using napoleon addon
+from __future__ import division, print_function, absolute_import
+
+__version__ = "3.6.0"
+
+__all__ = [
+    "AlkaliAtom",
+    "printState",
+    "printStateString",
+    "printStateStringLatex",
+    "printStateLetter",
+    "formatNumberSI",
+    "Hydrogen",
+    "Caesium",
+    "Cesium",
+    "Rubidium85",
+    "Rubidium",
+    "Rubidium87",
+    "Lithium6",
+    "Lithium7",
+    "Sodium",
+    "Potassium",
+    "Potassium39",
+    "Potassium40",
+    "Potassium41",
+    "Strontium88",
+    "Calcium40",
+    "Ytterbium174",
+    "Ylm",
+    "Wavefunction",
+    "StarkMap",
+    "LevelPlot",
+    "AtomSurfaceVdW",
+    "OpticalLattice1D",
+    "DynamicPolarizability",
+    "StarkBasisGenerator",
+    "ShirleyMethod",
+    "RWAStarkShift",
+    "PairStateInteractions",
+    "StarkMapResonances",
+    "Wigner3j",
+    "Wigner6j",
+    "TriaCoeff",
+    "CG",
+    "WignerDmatrix",
+    "C_k",
+    "C_c",
+    "C_h",
+    "C_e",
+    "C_m_e",
+    "getCitationForARC",
+    "Pulse"
+]
+from arc_sam.PulseClass import Pulse
+
+from arc_sam.alkali_atom_functions import (
+    AlkaliAtom,
+    printState,
+    printStateString,
+    printStateStringLatex,
+    printStateLetter,
+    formatNumberSI,
+)
+from arc_sam.alkali_atom_data import (
+    Hydrogen,
+    Caesium,
+    Cesium,
+    Rubidium85,
+    Rubidium,
+    Rubidium87,
+    Lithium6,
+    Lithium7,
+    Sodium,
+    Potassium,
+    Potassium39,
+    Potassium40,
+    Potassium41,
+)
+from arc_sam.divalent_atom_data import Strontium88, Calcium40, Ytterbium174
+from arc_sam.calculations_atom_single import (
+    Ylm,
+    Wavefunction,
+    StarkMap,
+    LevelPlot,
+    AtomSurfaceVdW,
+    OpticalLattice1D,
+    DynamicPolarizability,
+    StarkBasisGenerator,
+    ShirleyMethod,
+    RWAStarkShift,
+)
+from arc_sam.calculations_atom_pairstate import (
+    PairStateInteractions,
+    StarkMapResonances,
+)
+from arc_sam.wigner import Wigner3j, Wigner6j, TriaCoeff, CG, WignerDmatrix
+from arc_sam._database import getCitationForARC
+from arc_sam.PulseClass import Pulse
+from scipy.constants import k as C_k
+from scipy.constants import c as C_c
+from scipy.constants import h as C_h
+from scipy.constants import e as C_e
+from scipy.constants import m_e as C_m_e
