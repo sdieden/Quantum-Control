ad33db4b73310ccf864c050306cec3aca8c6f297
sdieden <75951267+sdieden@users.noreply.github.com>
2025-05-04 19:41:10 +0200

    commit before repparing everything...

--- SQUARESopti_multi_dt.py
+++ SQUARESopti_multi_dt.py
@@ -30,22 +30,22 @@ initial_wf = np.zeros(len(calc.basisStates),dtype=complex)
 initial_wf[calc.indexOfCoupledState]= 1
 
 # Définition des paramètres pour les champs électriques
-Emin = 14e2    # 800 V/m
-Emax = 16e2   # 1500 V/m
+Emin = 8e2    # 800 V/m
+Emax = 15e2   # 1500 V/m
 
-N = 50
+N = 200
 min_t_interval = 2e-10
 min_v_interval = 0.0029296875 * 100 #[V/m]
 step = round((Emax - Emin) / N)
-F_pos = np.linspace(Emin, Emax, num = 50)
-F_neg = np.linspace(-Emax, -Emin, num = 50)
+F_pos = np.linspace(Emin, Emax, num = 100)
+F_neg = np.linspace(-Emax, -Emin, num = 100)
 
 a = np.concatenate((F_pos, F_neg))
 a = np.sort(a)  # Trier les valeurs pour assurer l'ordre croissant
 
 # Liste des différentes durées de pulse à tester
 #dt_values = np.logspace(-7.221, -7.15, num = N)# Distribution logarithmique entre 10^-9 et 10^-6, avec plus de points
-dt_values = np.logspace(-7.221848,-7.154901959985743, num = N)
+dt_values = np.logspace(-9,-6, num = N)
 # Dictionnaires pour stocker les résultats pour chaque valeur de dt
 all_l10_populations = {}
 all_l_sup_10_populations = {}
---
+++ SQUARESopti_multi_pulses.py
@@ -0,0 +1,28 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import sys, os
+import cmath as cm
+import pathlib
+import time
+import datetime
+newModPath=pathlib.Path(os.path.dirname(os.path.abspath(__file__)),'NewModule')
+sys.path.insert(0, str(newModPath))
+from arc_sam import *  # Import ARC (Alkali Rydberg Calculator)
+from PulseFunction import U, apply_pulse, pulse_evolution, pulse_evolution_final, total_population,seconds_to_au , ghz_to_hartree, conv_ce, hbar
+from matplotlib.colors import LinearSegmentedColormap
+
+debut = time.time()
+
+# Initialization of the system
+atom = Calcium40()
+calc = StarkMap(atom)
+
+n = 35
+l = 3
+j = 3
+mj = 0
+s = 0
+nmin = n - 1
+nmax = n + 2
+lmax = nmax - 1
+calc.defineBasis(n, l, j, mj, nmin, nmax, lmax, progressOutput=True, s=s)
